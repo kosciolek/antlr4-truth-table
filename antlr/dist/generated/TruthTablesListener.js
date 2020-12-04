0b07860f5898f6ebb81a7258bac691ff2bc7739c
kosciolek <45483493+kosciolek@users.noreply.github.com>
2020-12-04 14:38:21 +0100

    rm antlr dist

--- antlr/dist/generated/TruthTablesListener.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/* eslint-disable */
-// @ts-nocheck
-// Generated from ./TruthTables.g4 by ANTLR 4.7.3-SNAPSHOT
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=TruthTablesListener.js.map
